74fe1793b97f4f9bd6c3c3d6e86560609866c26e
ArtemBotnev <arbotnev7@gmail.com>
2019-10-02 11:30:44 +0300

    added local

--- main.c
+++ main.c
@@ -2,8 +2,9 @@
 // Created by Artem Botnev on 26/09/2019.
 //
 
+#define WIN "windows"
 #if defined(_WIN32) || defined(_WIN64)
-    #define PLATFORM_NAME "windows"
+    #define PLATFORM_NAME WIN
 #else
     #define PLATFORM_NAME "not_bad_system_:)"
 #endif
@@ -12,7 +13,9 @@
 #define INN_ERROR_MESSAGE "ИНН должен быть из 12 цифр. Например: 009715225506"
 #define FACTORY_NUMBER_MESSAGE "Введите заводской номер и нажмите ввод"
 #define FACTORY_NUMBER_ERROR_MESSAGE "Заводской номер кассы должен быть из 14 цифр. Например: 00308300087104"
+#define RNM "РНМ"
 
+#include <locale.h>
 #include "crc.h"
 
 char inn[INN_SIZE];
@@ -20,6 +23,8 @@ char factory_number[FACTORY_NUMBER_SIZE];
 
 void ask_and_validate(char *src, short size, char *message, const char *error_message)
 {
+    setlocale(LC_ALL, "rus");
+
     char input[size + 2];
     printf("%s\n", message);
     fgets(input, size + 2, stdin);
@@ -42,9 +47,9 @@ int main() {
     ask_and_validate(factory_number, FACTORY_NUMBER_SIZE, FACTORY_NUMBER_MESSAGE, FACTORY_NUMBER_ERROR_MESSAGE);
 
     const char *answer = build_answer(inn, factory_number);
-    printf("РНМ: %s\n", answer);
+    printf("%s: %s\n", RNM,  answer);
 
-    if (PLATFORM_NAME == "windows") system("pause");
+    if (PLATFORM_NAME == WIN) system("pause");
 
     return 0;
 }
